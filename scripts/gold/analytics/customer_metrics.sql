a363a767e56497ebc0e53b1e0162172134caaeab
Aysenur Ucar <61329296+AysenurU@users.noreply.github.com>
2026-02-05 16:55:32 +0300

    Create customer_metrics.sql

---
+++ scripts/gold/analytics/customer_metrics.sql
@@ -0,0 +1,32 @@
+CREATE OR ALTER VIEW gold.customer_metrics
+AS
+SELECT
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	c.country,
+
+	COUNT(DISTINCT f.order_number) AS total_orders,
+	SUM(f.sales_amount)				AS lifetime_revenue,
+	-- Average Order Value (AOV)
+	ROUND(
+		SUM(f.sales_amount) * 1.0 / NULLIF( COUNT(DISTINCT f.order_number), 0),
+		2
+	) AS avg_order_value,
+
+	MIN(f.order_date)				AS first_order_date,
+	MAX(f.order_date)				AS last_order_date,
+
+	DATEDIFF(
+		DAY,
+		MAX(f.order_date),
+		GETDATE()
+	) AS days_since_last_order
+FROM gold.fact_sales f
+JOIN gold.dim_customers c
+	ON c.customer_key = f.customer_key
+GROUP BY c.customer_key,
+		c.first_name,
+		c.last_name,
+		c.country
+GO
